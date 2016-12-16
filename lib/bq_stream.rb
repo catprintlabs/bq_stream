761d160539600b1796834acbdcf44a85aed36911
Todd J Russell <russell@comexpressao.com>
2016-12-16 11:43:30 -0500

    update requires

--- lib/bq_stream.rb
+++ lib/bq_stream.rb
@@ -1,4 +1,5 @@
+require 'active_record'
 require 'bq_stream/version'
 require 'bq_stream/configuration'
+require 'bq_stream/queued_item'
 require 'bq_stream/bq_stream'
-require 'bq_stream/connection'
