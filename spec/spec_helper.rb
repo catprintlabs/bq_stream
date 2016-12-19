181076d38e8b387496f1ad4a2f62845222435864
Todd J Russell <russell@comexpressao.com>
2016-12-19 09:52:33 -0500

    adds timecop gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bq_stream'
 require 'pry'
+require 'timecop'
 
 ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
