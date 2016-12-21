192826f57e1e7391f57a4cd9b8ebb9bf82189883
Todd J Russell <russell@comexpressao.com>
2016-12-21 12:26:31 -0500

    rename dataset and update initialize for writing to BigQuery and dequeing items

--- lib/bq_stream/bq_stream.rb
+++ lib/bq_stream/bq_stream.rb
@@ -3,14 +3,31 @@
 module BqStream
   extend Configuration
 
-  def self.config_initialized
-    QueuedItem.build_table
-  end
-
   define_setting :client_id
   define_setting :service_email
   define_setting :key
   define_setting :project_id
-  define_setting :data_set
+  define_setting :dataset
   define_setting :bq_table_name, 'queued_items'
+
+  def self.config_initialized
+    QueuedItem.build_table
+    opts = {}
+    opts['client_id']     = client_id
+    opts['service_email'] = service_email
+    opts['key']           = key
+    opts['project_id']    = project_id
+    opts['dataset']       = dataset
+    @bq_writer = BigQuery::Client.new(opts)
+  end
+
+  def self.dequeue_items
+    BqStream::QueuedItem.all.each do |i|
+      @bq_writer.insert('table_name', table_name: i.table_name,
+                                      record_id: i.record_id, attr: i.attr,
+                                      new_value: i.new_value,
+                                      updated_at: Time.now)
+      BqStream::QueuedItem.destroy(i.id)
+    end
+  end
 end
