b4a035a44ff30d684a15020c71e9feb7f0e085f9
Todd J Russell <russell@comexpressao.com>
2017-01-02 10:16:42 -0500

    updates ar_earliest in OldestRecord table

--- lib/bq_stream/oldest_record.rb
+++ lib/bq_stream/oldest_record.rb
@@ -1,5 +1,27 @@
 module BqStream
   class OldestRecord < ActiveRecord::Base
+    def self.update_ar_earliest(&block)
+      OldestRecord.all.each { |old_rec| old_rec.update_oldest_records(&block) }
+    end
+
+    def update_oldest_records
+      destroy && return if older_records.empty?
+      older_records.each { |r| yield self, r }
+      update(bq_earliest_update: ar_records.first.updated_at)
+    end
+
+    def table_class
+      binding.pry
+      @table_class ||= table_name.constantize
+    end
+
+    def older_records
+      table_class.where(
+        'updated_at < ? AND updated_at >= ?',
+        bq_earliest_update || Time.now, BqStream.back_date
+      ).order('updated_at DESC').limit(BqStream.batch_size)
+    end
+
     def self.build_table
       self.table_name = BqStream.oldest_record_table_name
 
@@ -7,8 +29,7 @@ module BqStream
         t.string   :table_name
         t.string   :attr
         t.datetime :bq_earliest_update
-        t.datetime :ar_earliest_update
-      end unless connection.tables.include? table_name
+      end
     end
   end
 end
