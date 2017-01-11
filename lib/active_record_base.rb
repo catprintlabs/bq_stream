3b6f35e154e9ee4f5a9f9d4e7c6dc3b241421db6
Todd J Russell <russell@comexpressao.com>
2017-01-11 10:12:53 -0500

    update gem for use with opal

--- lib/active_record_base.rb
+++ lib/active_record_base.rb
@@ -1,26 +1,28 @@
 class ActiveRecord::Base
   def self.bq_attributes(opts = {})
-    if opts == :all
-      bq_atr_of_interest = column_names.map(&:to_sym)
-    elsif opts[:only]
-      raise 'opts must be an array' unless opts[:only].is_a? Array
-      bq_atr_of_interest = opts[:only].map(&:to_sym)
-    elsif opts[:except]
-      raise 'opts must be an array.' unless opts[:except].is_a? Array
-      bq_atr_of_interest = column_names.map(&:to_sym).select do |column|
-        !opts[:except].include?(column)
+    unless RUBY_ENGINE == 'opal'
+      if opts == :all
+        bq_atr_of_interest = column_names.map(&:to_sym)
+      elsif opts[:only]
+        raise 'opts must be an array' unless opts[:only].is_a? Array
+        bq_atr_of_interest = opts[:only].map(&:to_sym)
+      elsif opts[:except]
+        raise 'opts must be an array.' unless opts[:except].is_a? Array
+        bq_atr_of_interest = column_names.map(&:to_sym).select do |column|
+          !opts[:except].include?(column)
+        end
+      else
+        raise 'You must declare an opts hash with a key of :all, :only '\
+          'or :except) and a value as an array, if using :only or :except.'
       end
-    else
-      raise 'You must declare an opts hash with a key of :all, :only '\
-        'or :except) and a value as an array, if using :only or :except.'
-    end
-    bq_atr_of_interest.each do |attribute|
-      BqStream::OldestRecord
-        .find_or_create_by(table_name: name, attr: attribute)
-    end if BqStream.back_date
-    after_save { queue_item(bq_atr_of_interest) }
-    after_destroy do
-      BqStream::QueuedItem.create(table_name: self.class.to_s, record_id: id)
+      bq_atr_of_interest.each do |attribute|
+        BqStream::OldestRecord
+          .find_or_create_by(table_name: name, attr: attribute)
+      end if BqStream.back_date
+      after_save { queue_item(bq_atr_of_interest) }
+      after_destroy do
+        BqStream::QueuedItem.create(table_name: self.class.to_s, record_id: id)
+      end
     end
   end
 
@@ -32,5 +34,5 @@ class ActiveRecord::Base
                                     new_value: v[1].to_s)
       end
     end
-  end
+  end unless RUBY_ENGINE == 'opal'
 end
--- lib/bq_stream.rb
+++ lib/bq_stream.rb
@@ -1,10 +1,13 @@
-require 'dotenv'
-Dotenv.load
 require 'active_record'
 require 'active_record_base'
-require 'big_query'
-require 'bq_stream/version'
-require 'bq_stream/configuration'
-require 'bq_stream/queued_item'
-require 'bq_stream/oldest_record'
-require 'bq_stream/bq_stream'
+
+unless RUBY_ENGINE == 'opal'
+  require 'dotenv'
+  Dotenv.load
+  require 'big_query'
+  require 'bq_stream/version'
+  require 'bq_stream/configuration'
+  require 'bq_stream/queued_item'
+  require 'bq_stream/oldest_record'
+  require 'bq_stream/bq_stream'
+end
