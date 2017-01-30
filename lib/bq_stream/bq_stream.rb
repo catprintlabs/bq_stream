3beae6afbb2f808761704ce1ed26375a2b507b48
Todd J Russell <russell@comexpressao.com>
2017-01-30 15:08:01 -0500

    remove pry

--- lib/bq_stream/bq_stream.rb
+++ lib/bq_stream/bq_stream.rb
@@ -79,8 +79,7 @@ module BqStream
     log.info "#{Time.now}: [dequeue_items #{operation}] Starting..."
     create_bq_writer
     records = BqStream::QueuedItem.all.limit(BqStream.batch_size)
-    binding.pry
-    data = records.collect do |i|
+x    data = records.collect do |i|
       { table_name: i.table_name, record_id: i.record_id, attr: i.attr,
         updated_at: i.updated_at, new_value: i.new_value }
     end
