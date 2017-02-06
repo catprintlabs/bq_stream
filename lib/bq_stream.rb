0712476a5bb61d4ccba17b4260e5ebf9eafa2580
Todd J Russell <russell@comexpressao.com>
2017-02-06 14:33:03 -0500

    update to tests

--- lib/bq_stream.rb
+++ lib/bq_stream.rb
@@ -1,7 +1,7 @@
+require 'active_record'
 require 'active_record_base'
 
 unless RUBY_ENGINE == 'opal'
-  require 'active_record'
   require 'dotenv'
   Dotenv.load
   require 'big_query'
@@ -12,3 +12,14 @@ unless RUBY_ENGINE == 'opal'
   require 'bq_stream/bq_stream'
   Opal.append_path File.expand_path('../', __FILE__).untaint
 end
+
+# require 'active_record'
+# require 'active_record_base'
+# require 'dotenv'
+# Dotenv.load
+# require 'big_query'
+# require 'bq_stream/version'
+# require 'bq_stream/configuration'
+# require 'bq_stream/queued_item'
+# require 'bq_stream/oldest_record'
+# require 'bq_stream/bq_stream'
