ab33777b0a8cc6f0db1b2e8b422e566e7809ac92
Todd J Russell <russell@comexpressao.com>
2017-02-07 09:31:19 -0500

    expand on logging

--- lib/active_record_base.rb
+++ lib/active_record_base.rb
@@ -32,7 +32,9 @@ class ActiveRecord::Base
                "#{self.class} : #{id} : #{changes}"
     end
     changes.each do |k, v|
-      BqStream.log.info "#{Time.now}: [ID ATTRIBUTE] #{id}" if k == 'id'
+      if self.class.to_s == 'Order' && k == 'id'
+        BqStream.log.info "#{Time.now}: [ORDER ID IN CHANGES] #{id}"
+      end
       if attributes_of_interest.include?(k.to_sym)
         BqStream::QueuedItem.create(table_name: self.class.to_s,
                                     record_id: id, attr: k,
