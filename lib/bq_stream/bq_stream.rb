a16691860838607bc66d8994b3804ad8d6e42d74
Todd J Russell <russell@comexpressao.com>
2017-05-02 15:25:50 -0400

    remove logging

--- lib/bq_stream/bq_stream.rb
+++ lib/bq_stream/bq_stream.rb
@@ -49,7 +49,6 @@ module BqStream
                                    'as bq_earliest_update FROM '\
                                    "[#{project_id}:#{dataset}.#{bq_table_name}] "\
                                    'GROUP BY table_name, attr')
-    log.info "#{Time.now}: #{old_records['rows']}"
     old_records['rows'].each do |r|
       rec = OldestRecord.find_or_create_by(table_name: r['f'][0]['v'],
                                attr: r['f'][1]['v'])
