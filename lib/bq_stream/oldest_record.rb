102ce90e123e8b0b46feecc24f3ea83476519f92
Todd J Russell <russell@comexpressao.com>
2017-07-11 08:59:05 -0400

    logging

--- lib/bq_stream/oldest_record.rb
+++ lib/bq_stream/oldest_record.rb
@@ -27,17 +27,17 @@ module BqStream
     end
 
     def self.update_oldest_records_for(table)
-      BqStream.logger.info "#{Time.now}: Update Oldest Record #{logging_code}"
-      BqStream.logger.info "#{Time.now}: Table #{table} #{logging_code}"
+      BqStream.logger.info "#{Time.now}: Update Oldest Record"
+      BqStream.logger.info "#{Time.now}: Table #{table}"
       oldest_attr_recs = where('table_name = ?', table)
-      BqStream.logger.info "#{Time.now}: oldest_attr_recs count #{oldest_attr_recs.count} #{logging_code}"
+      BqStream.logger.info "#{Time.now}: oldest_attr_recs count #{oldest_attr_recs.count}"
       next_record = next_record_to_write(table.constantize, oldest_attr_recs.map(&:bq_earliest_update).uniq.min)
-      BqStream.logger.info "#{Time.now}: oldest_attr_recs #{next_record} #{logging_code}"
+      BqStream.logger.info "#{Time.now}: oldest_attr_recs #{next_record}"
       oldest_attr_recs.delete_all && return unless next_record
       oldest_attr_recs.each do |oldest_attr_rec|
         oldest_attr_rec.buffer_attribute(next_record)
       end
-      BqStream.logger.info "#{Time.now}: Updating All #{logging_code}"
+      BqStream.logger.info "#{Time.now}: Updating All"
       oldest_attr_recs.update_all(bq_earliest_update: next_record.created_at)
     end
 
