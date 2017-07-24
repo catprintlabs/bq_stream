756a0809b065f889c8a33335bb261de95816139f
Todd J Russell <russell@comexpressao.com>
2017-07-24 13:10:50 -0400

    rescue if needed

--- lib/bq_stream/oldest_record.rb
+++ lib/bq_stream/oldest_record.rb
@@ -25,7 +25,7 @@ module BqStream
     def self.update_oldest_records_for(table)
       BqStream.logger.info "#{Time.now}: >>>>> Update Oldest Records For #{table} Starting <<<<<"
       oldest_attr_recs = where('table_name = ?', table)
-      earliest_update = oldest_attr_recs.map(&:bq_earliest_update).uniq.min
+      earliest_update = oldest_attr_recs.map(&:bq_earliest_update).uniq.min rescue nil
       BqStream.logger.info "#{Time.now}: Table #{table} count #{oldest_attr_recs.count}"
       next_record = next_record_to_write(table.constantize, earliest_update)
       BqStream.logger.info "#{Time.now}: $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Earliest Time #{earliest_updaten} Blank? #{earliest_update.blank?} $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
